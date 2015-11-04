8f275dbd40e0e26b0bb006163b897c231ae1d5bf
Peter Ujvari <filc@Peters-MacBook-Pro-4.local>
2015-11-04 15:12:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# node-winston-logger-adapter
+
+This package is used for logging using winston.
+
+## Installation
+
+npm install winston-logger-adapter
---
+++ logger-adapter.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var winston = require('winston');
+var config;
+var logger;
+
+module.exports = {
+    initAdapter: (connector, _config) => {
+        config = _config;
+
+        logger = new (winston.Logger)({
+            transports: [
+                new (winston.transports.Console)()
+            ],
+            level: config.level
+        });
+    },
+
+    log: () => {
+        logger.log.apply(logger, Array.prototype.slice.call(arguments));
+    }
+};
